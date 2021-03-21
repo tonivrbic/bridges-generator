08bae406bb1210da55a80746cdc610f885147c02
Antonio Vrbić <antonio.vrbic@gdi.net>
2021-03-21 18:57:59 +0100

    rollup fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
-import typescript from "rollup-plugin-typescript2";
 import del from "rollup-plugin-delete";
 import dts from "rollup-plugin-dts";
+import typescript from "rollup-plugin-typescript2";
 
 export default [
   {
+    external: ["bridges-solver"],
     input: "./src/index.ts",
     output: [
       {
